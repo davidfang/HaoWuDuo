3c0deea0e5c520e6adcd6409049f7e8fd6c439fb
floadyun <floadyun@163.com>
2018-08-21 22:12:56 +0800

    增加首页tab切换

---
+++ haowuduo/HomeTab.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class HomeTab extends Component{
+
+
+    render (){
+        return (
+            <Text>这是详情页</Text>
+        );
+    }
+}
